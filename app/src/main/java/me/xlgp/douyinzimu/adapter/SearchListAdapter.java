1c6300050c46992075d91a827a7739d7368ee1e2
hzsn-csh <hzsn-csh@qq.com>
2021-08-27 16:45:22 +0800

    refactor:调整代码

---
+++ app/src/main/java/me/xlgp/douyinzimu/adapter/SearchListAdapter.java
@@ -0,0 +1,36 @@
+package me.xlgp.douyinzimu.adapter;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.function.Predicate;
+import java.util.stream.Collectors;
+
+public class SearchListAdapter<T> extends BaseAdapter<T> {
+    protected List<T> sourceList;
+    protected List<T> filterList;
+
+    public SearchListAdapter() {
+        this(new ArrayList<T>());
+    }
+
+    public SearchListAdapter(List<T> list) {
+        super(list);
+        setListData(list);
+    }
+
+    private void setListData(List<T> list) {
+        sourceList = list;
+        filterList = sourceList;
+    }
+
+    public void filter(Predicate<T> predicate) {
+        filterList = sourceList.stream().filter(predicate).collect(Collectors.toList());
+        super.updateData(filterList);
+    }
+
+    @Override
+    public void updateData(List<T> list) {
+        setListData(list);
+        super.updateData(filterList);
+    }
+}
