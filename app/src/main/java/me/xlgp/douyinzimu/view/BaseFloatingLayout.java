d39241b757c74ee5e9b28849f01de25277bbbf95
hzsn-csh <hzsn-csh@qq.com>
2021-09-10 14:26:53 +0800

    refactor:优化代码

--- app/src/main/java/me/xlgp/douyinzimu/view/BaseFloatingLayout.java
+++ app/src/main/java/me/xlgp/douyinzimu/view/BaseFloatingLayout.java
@@ -9,14 +9,12 @@ import me.xlgp.douyinzimu.obj.ZWindowManager;
 
 public class BaseFloatingLayout {
     private final Context context;
-    private View currentLayout;
+    private final View currentLayout;
     private String layoutName;
-    private WindowManager.LayoutParams layoutParams;
 
     public BaseFloatingLayout(Context context, Integer resource, WindowManager.LayoutParams layoutParams) {
         this.context = context;
         currentLayout = inflateLayout(resource);
-        this.layoutParams = layoutParams;
     }
 
     protected View inflateLayout(int resource) {
@@ -35,15 +33,10 @@ public class BaseFloatingLayout {
     protected void build(WindowManager.LayoutParams layoutParams, String key) {
         ZWindowManager.getInstance().addView(currentLayout, layoutParams, key);
         this.layoutName = key;
-        this.layoutParams = layoutParams;
     }
 
     protected void build(WindowManager.LayoutParams layoutParams) {
         build(layoutParams, "floatinglayout" + ZWindowManager.getInstance().count());
-    }
-
-    public WindowManager.LayoutParams getLayoutParams() {
-        return layoutParams;
     }
 
     public String getLayoutName() {
