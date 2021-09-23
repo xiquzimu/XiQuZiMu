29f0506708b6c1e4f4f047ac171f69ad57f38e7b
hzsn-csh <hzsn-csh@qq.com>
2021-09-23 14:18:19 +0800

    feat:新增IFetchRemoteRepository

---
+++ app/src/main/java/me/xlgp/douyinzimu/data/IFetchRemoteRepository.java
@@ -0,0 +1,11 @@
+package me.xlgp.douyinzimu.data;
+
+import java.util.List;
+
+import io.reactivex.rxjava3.core.Observable;
+
+public interface IFetchRemoteRepository {
+    Observable<List<String>> getNameList();
+
+    Observable<List<String>> changDuan(String path);
+}
