4bf3f4c840f6f09f4751d18aeec7d79f550d6bfa
hzsn-csh <hzsn-csh@qq.com>
2021-09-23 15:32:22 +0800

    refactor:优化代码

--- app/src/main/java/me/xlgp/douyinzimu/retrofit/RetrofitFactory.java
+++ app/src/main/java/me/xlgp/douyinzimu/retrofit/RetrofitFactory.java
@@ -4,29 +4,30 @@ import retrofit2.Retrofit;
 import retrofit2.adapter.rxjava3.RxJava3CallAdapterFactory;
 
 public class RetrofitFactory {
-    static String baseUrl = "https://gitee.com/xlgp/opera-lyrics/raw/master/";
 
-    public static <T> T fromGitee(Class<T> clazz) {
-        Retrofit retrofit = new Retrofit.Builder()
-                .addCallAdapterFactory(RxJava3CallAdapterFactory.create())
-                .addConverterFactory(StringListConverterFactory.create())
-                .baseUrl(baseUrl).build();
-        return retrofit.create(clazz);
-    }
-
-    /**
-     * github比较慢，使用jsdelivr中转
-     * baseUrl = https://cdn.jsdelivr.net/gh/{username}/{repository}@{branch}/{path}/{filename};
-     * @param tClass class
-     * @param <T> type
-     * @return T
-     */
-    public static <T> T fromGithub(Class<T> tClass){
-        String baseUrl = "https://cdn.jsdelivr.net/gh/xlgp/opera-lyrics@master/";
+    public static <T> T get(Class<T> tClass, String baseUrl) {
         Retrofit retrofit = new Retrofit.Builder()
                 .addCallAdapterFactory(RxJava3CallAdapterFactory.create())
                 .addConverterFactory(StringListConverterFactory.create())
                 .baseUrl(baseUrl).build();
         return retrofit.create(tClass);
+    }
+
+    public static <T> T fromGitee(Class<T> clazz) {
+        String baseUrl = "https://gitee.com/xlgp/opera-lyrics/raw/master/";
+        return get(clazz, baseUrl);
+    }
+
+    /**
+     * github比较慢，使用jsdelivr中转
+     * baseUrl = https://cdn.jsdelivr.net/gh/{username}/{repository}@{branch}/{path}/{filename};
+     *
+     * @param tClass class
+     * @param <T>    type
+     * @return T
+     */
+    public static <T> T fromGithub(Class<T> tClass) {
+        String baseUrl = "https://cdn.jsdelivr.net/gh/xlgp/opera-lyrics@master/";
+        return get(tClass, baseUrl);
     }
 }
