ab5794b0d36e0ad866d3cd95d1983a517588eb23
心灵的孤僻 <zhang15733@sina.cn>
2021-09-25 11:36:07 +0800

    refactor:更新点赞坐标

--- app/src/main/java/me/xlgp/douyinzimu/service/DianZanService.java
+++ app/src/main/java/me/xlgp/douyinzimu/service/DianZanService.java
@@ -27,7 +27,10 @@ public class DianZanService {
 
     private Point getPoint() {
         //不知道为什么点击屏幕底部会出现异常
-        return new Point(6, 200);
+        if (service == null) return new Point(6, 200);
+        Rect rect = new Rect();
+        service.getRootInActiveWindow().getBoundsInScreen(rect);
+        return new Point(8, rect.bottom - 300);
     }
 
     /**
