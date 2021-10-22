f747e71131b442aa6f6c03a041629009932edaf1
心灵的孤僻 <zhang15733@sina.cn>
2021-10-22 20:31:41 +0800

    feat:新增download service

---
+++ app/src/main/java/me/xlgp/xiquzimu/service/DownloadService.java
@@ -0,0 +1,107 @@
+package me.xlgp.xiquzimu.service;
+
+import android.app.DownloadManager;
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.content.IntentFilter;
+import android.net.Uri;
+import android.os.Environment;
+import android.os.IBinder;
+import android.widget.Toast;
+
+import androidx.annotation.Nullable;
+import androidx.lifecycle.LifecycleService;
+
+import org.jetbrains.annotations.NotNull;
+
+import java.io.File;
+
+import me.xlgp.xiquzimu.BuildConfig;
+import me.xlgp.xiquzimu.R;
+
+public class DownloadService extends LifecycleService {
+
+    private boolean downloading = false;
+    private String downloadUrl = null;
+    private String apkName = null;
+    private BroadcastReceiver receiver;
+
+    @Nullable
+    @Override
+    public IBinder onBind(@NotNull Intent intent) {
+        return super.onBind(intent);
+    }
+
+    @Override
+    public int onStartCommand(Intent intent, int flags, int startId) {
+        if (downloading) {
+            Toast.makeText(this, "正在下载", Toast.LENGTH_SHORT).show();
+            return super.onStartCommand(intent, flags, startId);
+        }
+
+        downloadUrl = intent.getStringExtra("apkDownloadUrl");
+        apkName = downloadUrl.substring(downloadUrl.lastIndexOf("/") + 1);
+        if (!checkVersion(apkName)) {
+            Toast.makeText(this, "无版本可更新", Toast.LENGTH_SHORT).show();
+            stopSelf();
+            return super.onStartCommand(intent, flags, startId);
+        }
+
+        receiver = new DownloadBroadcastReceiver();
+
+        registerReceiver(receiver, new IntentFilter(DownloadManager.ACTION_DOWNLOAD_COMPLETE));
+
+        startDownload();
+
+        return super.onStartCommand(intent, flags, startId);
+    }
+
+    private boolean checkVersion(String apkName) {
+        String[] list = apkName.split("_");
+        if (list.length <= 1) {
+            return false;
+        }
+        String version = list[1].substring(1);
+        return BuildConfig.VERSION_NAME.compareTo(version) < 0;
+    }
+
+    @Override
+    public void onDestroy() {
+        if (receiver != null) {
+            unregisterReceiver(receiver);
+        }
+        super.onDestroy();
+    }
+
+    private void startDownload() {
+        Uri mApkUri = Uri.parse("https://gitee.com" + downloadUrl);
+
+        DownloadManager.Request request = new DownloadManager.Request(mApkUri);
+
+        request.setAllowedNetworkTypes(DownloadManager.Request.NETWORK_MOBILE | DownloadManager.Request.NETWORK_WIFI);
+        request.setTitle(getString(R.string.app_zh_name));
+        request.setDescription("在正下载 " + apkName + " ...");
+        request.setNotificationVisibility(DownloadManager.Request.VISIBILITY_VISIBLE_NOTIFY_COMPLETED);
+        request.setVisibleInDownloadsUi(true);
+        request.setDestinationInExternalFilesDir(this, Environment.DIRECTORY_DOWNLOADS, apkName);
+
+        DownloadManager dm = (DownloadManager) getSystemService(Context.DOWNLOAD_SERVICE);
+
+        dm.enqueue(request);
+        downloading = true;
+    }
+
+    class DownloadBroadcastReceiver extends BroadcastReceiver {
+
+        @Override
+        public void onReceive(Context context, Intent intent) {
+            intent = new Intent(Intent.ACTION_VIEW);
+            intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+            intent.setDataAndType(Uri.fromFile(new File(Environment.DIRECTORY_DOWNLOADS + apkName)),
+                    "application/vnd.android.package-archive");
+            startActivity(intent);
+            stopSelf();
+        }
+    }
+}
--- app/src/main/java/me/xlgp/xiquzimu/ui/about/AboutFragment.java
+++ app/src/main/java/me/xlgp/xiquzimu/ui/about/AboutFragment.java
@@ -1,15 +1,11 @@
 package me.xlgp.xiquzimu.ui.about;
 
-import android.app.DownloadManager;
-import android.content.Context;
-import android.net.Uri;
+import android.content.Intent;
 import android.os.Bundle;
-import android.os.Environment;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
@@ -17,11 +13,8 @@ import androidx.lifecycle.ViewModelProvider;
 
 import com.google.android.material.snackbar.Snackbar;
 
-import java.util.List;
-
-import me.xlgp.xiquzimu.BuildConfig;
-import me.xlgp.xiquzimu.R;
 import me.xlgp.xiquzimu.databinding.FragmentAboutBinding;
+import me.xlgp.xiquzimu.service.DownloadService;
 
 public class AboutFragment extends Fragment {
 
@@ -35,54 +28,18 @@ public class AboutFragment extends Fragment {
         View root = binding.getRoot();
 
         viewModel.getPackageVersion().observe(getViewLifecycleOwner(), s -> binding.textPackageVersion.setText(s));
-        viewModel.errorState.observe(getViewLifecycleOwner(), s -> {
-            Snackbar.make(binding.getRoot(), s, Snackbar.LENGTH_SHORT).show();
-        });
+        viewModel.errorState.observe(getViewLifecycleOwner(), s -> Snackbar.make(binding.getRoot(), s, Snackbar.LENGTH_SHORT).show());
 
         viewModel.getDownloadUrlList().observe(getViewLifecycleOwner(), list -> {
             Log.i("TAG", "onCreateView: " + list);
-            download(list);
+            Intent intent = new Intent(requireActivity(), DownloadService.class);
+            intent.putExtra("apkDownloadUrl", list.get(0));
+            requireActivity().startService(intent);
         });
 
         binding.updateVersion.setOnClickListener(v -> viewModel.loadDownloadUrl());
 
         return root;
-    }
-
-    private boolean checkVersion(String apkName) {
-        String[] list = apkName.split("_");
-        if (list.length <= 1) {
-            return false;
-        }
-        String version = list[1].substring(1);
-        ;
-        return BuildConfig.VERSION_NAME.compareTo(version) < 0;
-    }
-
-    private void download(List<String> list) {
-        DownloadManager mDownloadManager = (DownloadManager) requireActivity().getSystemService(Context.DOWNLOAD_SERVICE);
-
-        String url = list.get(0);
-        String apkName = list.get(0).substring(list.get(0).lastIndexOf("/") + 1);
-
-        if (!checkVersion(apkName)) {
-            Toast.makeText(requireActivity(), "无版本可更新", Toast.LENGTH_SHORT).show();
-            return;
-        }
-
-        String mApkDir = "Download/" + apkName;
-
-        Uri mApkUri = Uri.parse("https://gitee.com" + url);
-        DownloadManager.Request down = new DownloadManager.Request(mApkUri);
-
-        down.setAllowedNetworkTypes(DownloadManager.Request.NETWORK_MOBILE | DownloadManager.Request.NETWORK_WIFI);
-        down.setTitle(requireActivity().getString(R.string.app_zh_name));
-        down.setDescription("在正下载 " + apkName + " ...");
-        down.setNotificationVisibility(DownloadManager.Request.VISIBILITY_VISIBLE_NOTIFY_COMPLETED);
-        down.setVisibleInDownloadsUi(true);
-        down.setDestinationInExternalFilesDir(requireActivity(), Environment.DIRECTORY_DOWNLOADS, mApkDir);
-
-        long mDownloadId = mDownloadManager.enqueue(down);
     }
 
     @Override
